93da7daf188b0536d6f2a22bf745b6f9d6820544
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-19 22:23:08 +0530

    Create count.c

---
+++ SET1/count.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("ENTER THE INTEGER: ");
+	scanf("%d",&n);
+	printf("TO COUNT THE NUMBER OF DIGITS IN THE INTEGER :");
+	printf("\t%d",printf("%d",n));
+}
