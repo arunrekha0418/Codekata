0d47cf7442a07165baf54fff5f8db9c00d255df6
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-19 10:31:25 +0000

    Create odd_even.c

---
+++ SET1/odd_even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int num;
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("\nthe given number %d is EVEN",num);
+}
+else
+{
+printf("\nthe given number %d is ODD",num);
+}
+}
