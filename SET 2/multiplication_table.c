4a1f68fd583c5d4bccb74b303b70655eae870de4
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-20 08:11:36 +0530

    Create multiplication_table.c

---
+++ SET 2/multiplication_table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,a;
+	printf("ENTER THE NUMBER TO PRINT THE MULTIPLICATION TABLE:\n");
+	scanf("%d",&a);
+	printf("ENTER THE NO.OF TIMES TO PRINT THE MULTIPLICATION TABLE:\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("\n%d * %d=%d",a,i,a*i);
+	}
+	
+}
