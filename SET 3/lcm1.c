427de9777779521d96db96f7b58e409b41617c88
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-20 21:51:04 +0530

    Create lcm1.c

---
+++ SET 3/lcm1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,max=0,gcd=0,i,lcm;
+	printf("ENTER THE TWO INTEGERS :\n");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		max=a;
+	}
+	else
+	{
+		max=b;
+	}
+	for(i=2;i<max;i++)
+	{
+		if(((a%i)==0)&&((b%i)==0))
+		{
+			gcd=i;
+		}
+	}
+	lcm=(a*b)/gcd;
+	printf("THE LCM FOR %d AND %d IS %d",a,b,lcm);
+}
