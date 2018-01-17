c9d40e3ff159e44c695117b700cdaf511b64ac83
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-17 21:10:56 +0530

    Create powernum.c

---
+++ SET 5/powernum.c
@@ -0,0 +1,22 @@
+//to check wether the given number is a power of 2 or not.
+
+#include<stdio.h>
+void main()
+{
+	int n,i,count=0,j=1;
+	printf("ENTER THE NUMBER TO CHECK WHETHER IT IS POWER OF 2 OR NOT\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		j=j*2;
+	if(j==n)
+		count=1;
+	}
+	if(count==1)
+	{
+			printf("\nYES");
+	}
+	else
+		printf("\nNO");
+	
+}
