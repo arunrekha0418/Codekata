d5b7be885829e70002927a18804d97dfdf54d0f3
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-17 21:17:18 +0530

    Create sum.c

---
+++ SET 5/sum.c
@@ -0,0 +1,17 @@
+//to find the sum of two nos
+
+#include<stdio.h>
+int sum(int a,int b)
+{
+	int sum=0;
+	sum=a+b;
+	printf("%d",sum);
+	return 0;
+}
+void main()
+{
+int a,b;
+printf("TO FIND THE SUM OF TWO NOS\n");
+	scanf("%d%d",&a,&b);
+	sum(a,b);
+}
