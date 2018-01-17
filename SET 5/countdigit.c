6d4a6dfe46dc11a6ed80fc7b549834358ba8ddb1
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-17 21:47:30 +0530

    Create countdigit.c

---
+++ SET 5/countdigit.c
@@ -0,0 +1,10 @@
+//to count the no.of digits in the given number.
+
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("ENTER THE NUMBER\n");
+	scanf("%d",&n);
+	printf("\nTHE NO.OF.DIGITS IN THE GIVEN NUMBER IS %d",printf("%d",n));
+}
