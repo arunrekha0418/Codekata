67cc6c99fcc7f123fd6f4c0e3b907bcdb130e6f6
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-19 22:27:56 +0530

    Create pro_evenodd.c

---
+++ SET 6/pro_evenodd.c
@@ -0,0 +1,15 @@
+//to find whether the product of 2nos as odd or even
+
+#include<stdio.h>
+void main()
+{
+	int n,m,pro;
+	printf("ENTER THE INPUT NOS\n");
+	scanf("%d%d",&n,&m);
+	pro=n*m;
+	if((pro%2)==0)
+	printf("EVEN");
+	else
+	printf("ODD");
+	
+}
