3b9df18736c7c4104a76e23301a92110cfff95b9
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-19 22:21:13 +0530

    Create even.c

---
+++ SET 6/even.c
@@ -0,0 +1,13 @@
+//to print the nearest even number which is less than or equal to.
+
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("ENTER THE INPUT NUMBER\n");
+	scanf("%d",&n);
+	if((n%2)==0)
+	printf("THE NEAREST EVEN NUMBER IS %d",n);
+	else
+	printf("THE NEAREST EVEN NUMBER IS %d",n-1);
+}
