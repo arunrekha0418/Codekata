0ea8027131bbf4c8f79ab30ad44c3a43faa3ce9a
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-01-21 13:49:29 +0530

    Create fibo.c

---
+++ SET 6/fibo.c
@@ -0,0 +1,18 @@
+//c program to print the fibonacci series
+
+#include<stdio.h>
+void main()
+{
+	int n,c,a=1,b=0;
+	printf("ENTER THE NO.OF.REPETATION\n");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+		n--;
+		printf(" %d",c);
+		
+	}
+}
