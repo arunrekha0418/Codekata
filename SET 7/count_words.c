3758554f983789aa57238ec6b660153ffbe274b5
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-02-05 08:03:52 +0530

    Create count_words.c

---
+++ SET 7/count_words.c
@@ -0,0 +1,20 @@
+//63.to count the no of words in the given string..
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+void main()
+{
+	char s1[50];
+	int n,words=1,i;
+	printf("ENTER THE INPUT STRING:\n");
+	gets(s1);
+	n=strlen(s1);
+	//printf("%d",n);
+	for(i=0;i<n;i++)
+	{
+		if(isspace(s1[i]))
+		words++;
+	}
+	printf("THE NO OF WORDS IN THE GIVEN STRING IS :n %d",words);
+}
