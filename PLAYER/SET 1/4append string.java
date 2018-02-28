0edaf3e4d9a935fc9eb611ad4d9c2d807c237b67
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-02-28 22:02:07 +0530

    Create 4append string.java

---
+++ PLAYER/SET 1/4append string.java
@@ -0,0 +1,14 @@
+//4.given a string append '.'
+
+import java.util.Scanner;
+class append
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+String a;
+System.out.println("ENTER THE INPUT STRING");
+a=in.nextLine();
+System.out.println("BEFORE APPENDING: " + a);
+System.out.println("AFTER APPENDING: " + a + ".");
+}}
