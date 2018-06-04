23a0d4b00d0161728c2583c94e296ddaf9af7952
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-04 23:27:42 +0530

    Create 46sineangle.java

---
+++ PLAYER/SET 5/46sineangle.java
@@ -0,0 +1,14 @@
+//46 to print the sine angle...
+
+import java.util.*;
+class sine
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+long degree;
+long sinangle;
+System.out.println("ENTER THE ANGLE:");
+degree=in.nextLong();
+System.out.println("THE SINGLE OF " + degree + " IS " + Math.sin(degree));
+}}
