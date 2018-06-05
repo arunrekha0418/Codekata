4a4941753ff00c86e18824399fe18027a8617dac
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-05 19:08:08 +0530

    Create 84.notoperator.java

---
+++ PLAYER/SET 9/84.notoperator.java
@@ -0,0 +1,14 @@
+//84.to print th eresult of not operation of an integer
+
+import java.util.*;
+class notop
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int n,res=0;
+System.out.println("ENTER THE INPUT INTEGER:");
+n=in.nextInt();
+res=~n;
+System.out.println("THE RESULT OF NOT OPERATION IS "  +  res);
+}}
