924f0c261389cccdff64a108abf3627f45d3df41
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-06 13:30:33 +0530

    Create 87.leftshift.java

---
+++ PLAYER/SET 9/87.leftshift.java
@@ -0,0 +1,15 @@
+//87.leftshift operation...
+
+import java.util.*;
+class leftshift
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int n,k,res=0;
+System.out.println("ENTER THE INPUTS:");
+n=in.nextInt();
+k=in.nextInt();
+res=n << k;
+System.out.println("THE RESULT IS : " + res);
+}}  
