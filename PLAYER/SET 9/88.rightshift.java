1fb404c11d8c61516915bcdfdc0e2230e2efd616
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-06 13:32:03 +0530

    Create 88.rightshift.java

---
+++ PLAYER/SET 9/88.rightshift.java
@@ -0,0 +1,17 @@
+//88.rightshift operation...
+
+
+import java.util.*;
+class rightshift
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int n,k;
+float res=0;
+System.out.println("ENTER THE INPUTS:");
+n=in.nextInt();
+k=in.nextInt();
+res=n >> k;
+System.out.println("THE RESULT IS : " + res);
+}}  
