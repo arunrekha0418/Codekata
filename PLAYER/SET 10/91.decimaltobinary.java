564d65d60df569f96fa3f8cddd91182d9f571f64
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-10 20:21:38 +0530

    Create 91.decimaltobinary.java

---
+++ PLAYER/SET 10/91.decimaltobinary.java
@@ -0,0 +1,15 @@
+//91.decimal to binary conversion..
+//note : input should be integer...
+
+import java.util.*;
+class decbin
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int a;
+System.out.println("ENTE THE INPUT:");
+a=in.nextInt();
+System.out.println("THE BINARY EQUIVALENT FOR GIVEN DECIMAL IS:");
+System.out.println(Integer.toBinaryString(a));
+}}
