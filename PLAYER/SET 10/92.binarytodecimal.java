588ad1e1b1645800f74262ddda64815d1b842362
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-10 20:20:25 +0530

    Create 92.binarytodecimal.java

---
+++ PLAYER/SET 10/92.binarytodecimal.java
@@ -0,0 +1,14 @@
+//92.to convert binary to decimal
+
+import java.util.*;
+class bindec
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+String a;
+System.out.println("ENTER THE INPUT:");
+a=in.next();
+System.out.println("THE DECIMAL EQUIVALENT FOR GIVEN BINARY IS:");
+System.out.println(Integer.parseInt(a,2));
+}}
