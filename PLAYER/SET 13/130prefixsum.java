133340d56ac245d49d7feca189b9de92c55ab195
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-13 13:20:34 +0530

    Create 130prefixsum.java

---
+++ PLAYER/SET 13/130prefixsum.java
@@ -0,0 +1,30 @@
+//130.to print the prefix sum array for the given array
+
+import java.util.*;
+class prefixsum
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int n,i,sum=0,k=0;
+System.out.println("ENTER THE SIZE OF THE ARRAY:");
+n=in.nextInt();
+System.out.println("ENTER  THE ARRAY ELEMENTS:");
+int a[]=new int[n];
+int b[]=new int[n];
+
+for(i=0;i<n;i++)
+{
+a[i]=in.nextInt();
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+b[k]=sum;
+k++;
+}
+for(i=0;i<k;i++)
+{
+System.out.print(b[i] + " ");
+}
+}}
