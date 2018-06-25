ff72d133a01b9fc06f0aa8006a6547ee8e691f10
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-06-25 19:16:57 +0530

    Create 139.ones.java

---
+++ PLAYER/SET 14/139.ones.java
@@ -0,0 +1,13 @@
+//139.to print the no.of.ones in the  given input...
+
+import java.util.*;
+class ones
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+String a;
+System.out.println("ENTER THE INPUT:");
+a=in.next();
+System.out.println(a.length());
+}}
