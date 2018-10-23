dca3488f1833e4c66bb0ad38e03ca41f38479257
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-10-23 22:01:28 +0530

    Create 193.to find ax+b.java

---
+++ PLAYER/SET 20/193.to find ax+b.java
@@ -0,0 +1,18 @@
+package guviplayer;
+import java.util.*;
+public class Set20_193 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a,x,b,y;
+		System.out.println("ENTER THE INPUTS:");
+		a=in.nextInt();
+		b=in.nextInt();
+		x=in.nextInt();
+		y=(a*x)+b;
+		System.out.println("the ans for the solution ax+b is : " + y);
+		in.close();
+
+	}
+
+}
